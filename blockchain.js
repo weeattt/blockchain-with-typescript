609edc47a9df1b3ad7577f7c6157d858ba1a4e2b
weeattt <weeat0420@naver.com>
2022-09-05 21:50:06 +0900

    first commit

---
+++ blockchain.js
@@ -0,0 +1,107 @@
+📌 Types of TS(기본)
+✅ 배열: 자료형[]
+✅ 숫자: number
+✅ 문자열: string
+✅ 논리: boolean
+✅ optional
+const player : {
+    name: string,
+    age?:number
+} = {
+    name: "nico"
+}
+
+❌ player.age가 undefined일 가능성 알림
+if(player.age < 10) {
+}
+
+⭕ player.age가 undefined일 가능성 체크
+if(player.age && player.age < 10) {
+}
+
+❗ ?를 :앞에 붙이면 optional
+
+✅ Alias(별칭) 타입
+type Player = {
+    name: string,
+    age?:number
+}
+
+const player : Player = {
+    name: "nico"
+}
+
+⭐ 함수에서는 어떻게 쓸까
+type Player = {
+    name: string,
+    age?:number
+}
+
+function playerMaker1(name:string) : Player {
+    return {
+        name
+    }
+}
+
+const playerMaker2 = (name:string) : Player => ({name})
+
+const nico = playerMaker1("nico")
+nico.age = 12
+
+📌 Types of TS(part II)
+✅ readonly 사용하기
+type Player = {
+    readonly name:string
+    age?:number
+}
+
+const playerMaker = (name: string): Player => ({name})
+
+const nico = playerMaker("nico")
+🚫 nico.name = "aa"
+
+const numbers: readonly number[] = [1, 2, 3, 4]
+🚫 numbers.push(1)
+❗ readonly가 있으면 최초 선언 후 수정 불가
+    ⇒ immutability(불변성) 부여
+        but, javascript에서는 그냥 배열
+
+✅ Tuple
+정해진 개수와 순서에 따라 배열 선언
+const player: [string, number, boolean] = ["nico", 1, true]
+❗ readonly도 사용가능 ⇒ readonly [...] 형태
+
+✅ undefined, null, any
+any: 아무 타입
+undefined: 선언X 할당X
+null: 선언O 할당X
+
+void
+void는 값을 반환하지 않는 함수의 반환 값을 나타냅니다. 함수에 return 문이 없거나 해당 return 문에서 명시적 값을 반환하지 않을 때 항상 유추되는 타입입니다.
+```
+// The inferred return type is void
+function noop() {
+return;
+}
+```
+unknown
+unknown타입은 모든 값을 나타냅니다. 이것은 any타입과 비슷하지만 any보다 unknown이 더 안전합니다. 이유는 unknown값으로 작업을 수행하는 것은 합법적이지 않기 때문입니다.
+```
+function hello(a: any) {
+a.b(); // OK
+}
+
+function hello2(a: unknown) {
+a.b(); // 에러: Object is of type 'unknown'.
+}
+```
+never
+일부 함수는 값을 반환하지 않습니다.
+이는 함수가 예외를 throw하거나 프로그램 실행을 종료함을 의미합니다.
+```
+function fail(msg: string): never {
+throw new Error(msg);
+}
+```
+https://www.typescriptlang.org/docs/handbook/2/functions.html#unknown
+
